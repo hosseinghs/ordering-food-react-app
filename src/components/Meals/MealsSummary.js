83f7f345e530a8e11c47d2089417d382d5d6fa88
Hossein Samani <samanihossein1379@gmail.com>
2022-07-27 21:30:10 +0430

    meals added

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,21 @@
+const MealsSummary = () => {
+  return (
+    <section className="w-1/2 mx-auto bg-black text-white p-4 text-center shadow-md rounded-xl ">
+      <h2 className="text-xl font-extrabold mb-6">
+        Delicious Food, Delivered To You
+      </h2>
+      <div className="text-sm">
+        <p className="mb-4">
+          Choose your favorite meal from our broad selection of available meals
+          and enjoy a delicious lunch or dinner at home.
+        </p>
+        <p>
+          All our meals are cooked with high-quality ingredients, just-in-time
+          and of course by experienced chefs!
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default MealsSummary;
