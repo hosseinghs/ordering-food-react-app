fae935afff7b9085484903147d04960418d44fd4
Hossein Samani <samanihossein1379@gmail.com>
2022-07-31 00:14:20 +0430

    number of items in header fixed

--- src/components/Layout/HeaderCartBtn.js
+++ src/components/Layout/HeaderCartBtn.js
@@ -1,5 +1,14 @@
+import { useContext } from "react";
+import CartContext from "../../context/Cart";
+
 import CartIcon from "../Cart/CartIcon";
 const HeaderCartBtn = () => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfVal = cartCtx.items.reduce((currentNum, item) => {
+    return currentNum + item.amount;
+  }, 0);
+
   return (
     <button className="flex items-center px-6 py-2 bg-red-800 rounded-full">
       <span>
@@ -7,7 +16,7 @@ const HeaderCartBtn = () => {
       </span>
       <span className="text-sm font-semibold mx-4">Your Cart</span>
       <span className="text-sm font-semibold rounded-full bg-red-300 px-2 py-1">
-        3
+        {numberOfVal}
       </span>
     </button>
   );
