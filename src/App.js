d6d32aa91b2c38b7dcb424822177c8f240913b74
Hossein Samani <samanihossein1379@gmail.com>
2022-07-31 00:14:07 +0430

    app wrapped in context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import CartProvider from "./context/CartProvicer";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
@@ -6,13 +7,13 @@ import Cart from "./components/Cart/Cart";
 function App() {
   const [showCart, setShowCart] = useState(false);
   return (
-    <div className="App">
+    <CartProvider className="App">
       {showCart && <Cart onCartClose={() => setShowCart(false)} />}
       <Header onCartClick={() => setShowCart(true)} />
       <main>
         <Meals />
       </main>
-    </div>
+    </CartProvider>
   );
 }
 
