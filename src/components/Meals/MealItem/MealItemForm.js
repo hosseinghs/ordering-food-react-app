d38db7b964a36560212fbe7b966e8e9814e72ae6
Hossein Samani <samanihossein1379@gmail.com>
2022-07-30 19:51:06 +0430

    MealItemForm component added

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,22 @@
+import Input from "../../UI/Input";
+
+const MealItemForm = () => {
+  return (
+    <form className="">
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button type="submit">+ Add</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
